85aac6d382a7a4979264a8d16498146b47e91c40
dahoon06 <dahoon226@naver.com>
2024-09-27 16:00:19 +0900

    chore: Update next config -test

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const prefix =
   process.env.NODE_ENV === 'production' ? 'https://dahoon06.github.io/notion-blog/' : ''
 
 module.exports = {
+  output: 'export',
   images: {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
   },
