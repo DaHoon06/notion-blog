be8b7dd10273e86169d37a6d71a6ec7742733d4c
dahoon06 <dahoon226@naver.com>
2024-09-30 11:21:09 +0900

    # next config rollback

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
-const prefix =
-  process.env.NODE_ENV === 'production' ? 'https://dahoon06.github.io/notion-blog/' : ''
-
 module.exports = {
-  output: 'export',
-  basePath: '/notion-blog',
   images: {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
   },
-  assetPrefix: prefix,
-  trailingSlash: true,
 }
