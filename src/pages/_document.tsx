013f1592938ec0a1bd95c30bcee295e93896aa27
dahoon06 <dahoon226@naver.com>
2025-01-05 23:35:18 +0900

    fix: 오타수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -36,8 +36,11 @@ class MyDocument extends Document {
               />
             </>
           )}
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9259748218576901"
-     crossorigin="anonymous"></script>
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9259748218576901"
+            crossOrigin="anonymous"
+          ></script>
         </Head>
         <body>
           <Main />
